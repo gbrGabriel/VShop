7aab73962358117944809eababaf674a5670e22f
Gabriel Silva <gabriel.silva@JNMOURA.JNMOURA>
2024-04-09 15:34:20 -0300

    Adicionado implementações dos repositories

---
+++ src/VShop.Product/Repositories/RepositoryBase.cs
@@ -0,0 +1,113 @@
+using Context;
+using Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories;
+public abstract class RepositoryBase<TEntity>(ApplicationDbContext context) : IRepositoryBase<TEntity> where TEntity : class
+{
+    public readonly ApplicationDbContext _context = context;
+
+    public async Task<int> DeleteAsync(TEntity entity)
+    {
+        int result = 0;
+
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                _context.Set<TEntity>().Remove(entity);
+
+                result = await _context.SaveChangesAsync();
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+        return result;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        int result = 0;
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                result = await _context.SaveChangesAsync();
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+        return result;
+    }
+
+    public async Task<int> UpdateAsync(TEntity entity)
+    {
+        int result = 0;
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                _context.Set<TEntity>().Update(entity);
+
+                result = await _context.SaveChangesAsync();
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction?.Rollback();
+                throw;
+            }
+        }
+        return result;
+    }
+
+    public void Add(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        foreach (var entity in entities)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _context.Set<TEntity>().Add(entity);
+            }
+            else
+            {
+                _context.Set<TEntity>().Attach(entity);
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+        }
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync() =>
+     await _context.Set<TEntity>().ToListAsync();
+
+    public virtual async Task<TEntity?> GetByIdAsync(Guid id) =>
+         await _context.Set<TEntity>().FindAsync(id);
+
+    public void Dispose()
+    {
+        _context.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
