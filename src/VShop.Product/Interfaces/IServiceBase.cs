18d38ccc5c7d33cd7b268958fd0766b445ec7207
Gabriel Silva <gabriel.silva@JNMOURA.JNMOURA>
2024-04-11 15:32:35 -0300

    Alteração na implementação do service base

--- src/VShop.Product/Interfaces/IServiceBase.cs
+++ src/VShop.Product/Interfaces/IServiceBase.cs
@@ -7,6 +7,8 @@ public interface IServiceBase<TEntity, TDto>
     Task<int> SaveChangesAsync();
     Task<int> UpdateAsync(TDto dto);
     Task<int> DeleteAsync(TDto dto);
+    Task<TDto> AddSaveAsync(TDto dto);
+    Task<TDto> AddUpdateAsync(TDto dto);
     void Add(TDto dto);
     void AddRange(IEnumerable<TDto> dtos);
     Task<TDto?> GetByIdAsync(int id);
