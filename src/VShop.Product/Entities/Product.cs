593a995aebee7ef4c905c086ddca2080a19219b3
Gabriel Silva <gabriel.silva@JNMOURA.JNMOURA>
2024-04-11 15:30:28 -0300

    Alteração na entidade de product

--- src/VShop.Product/Entities/Product.cs
+++ src/VShop.Product/Entities/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public decimal Price { get; set; }
-    public string Descripton { get; set; } = null!;
+    public string Description { get; set; } = null!;
     public long Stock { get; set; }
     public string? ImageUrl { get; set; }
     public int CategoryId { get; set; }
