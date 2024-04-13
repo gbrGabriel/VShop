c50db0aed83126e923b99e8ca4da6429a8419c04
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-13 15:46:27 -0300

    Alterações nos DTOs da aplicação web

---
+++ src/VShop.Web/DTOs/CategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VShopWeb.DTOs;
+
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<ProductDTO>? Products { get; set; }
+}
---
+++ src/VShop.Web/DTOs/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace VShopWeb.DTOs;
+
+public class ProductDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = null!;
+    public long Stock { get; set; }
+    public string ImageUrl { get; set; } = null!;
+    public string CategoryName { get; set; } = null!;
+    public int CategoryId { get; set; }
+}
