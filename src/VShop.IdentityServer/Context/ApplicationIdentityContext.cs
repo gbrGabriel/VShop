685f343fd1ec6bfe3f33407b64fc0a6d9238d0e7
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-14 22:41:32 -0300

    Adicionado projeto para identity

---
+++ src/VShop.IdentityServer/Context/ApplicationIdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VShopIdentityServer.Identity;
+
+namespace VShopIdentityServer.Context;
+
+public class ApplicationIdentityContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationIdentityContext(DbContextOptions<ApplicationIdentityContext> options) : base(options)
+    {
+
+    }
+}
---
+++ src/VShop.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+#nullable enable
+namespace VShop.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
