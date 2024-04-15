44ce9a69b4b27737bf95e4dd9482894473443e2c
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-14 22:43:37 -0300

    Adicionado novs endpoints

--- src/VShop.Web/Controllers/ProductController.cs
+++ src/VShop.Web/Controllers/ProductController.cs
@@ -26,28 +26,119 @@ public class ProductController(IServiceProduct serviceProduct, IServiceCategory 
     [HttpGet]
     public async Task<IActionResult> CreateProduct()
     {
-        ViewBag.CategoryId = new
+        try
+        {
+            ViewBag.CategoryId = new
+                      SelectList(await _serviceCategory.GetAllAsync("/api/v1/category"), "Id", "Name");
+            return View();
+        }
+        catch (Exception)
+        {
+            return View("Error");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateProduct(int id)
+    {
+        try
+        {
+            ViewBag.CategoryId = new
                         SelectList(await _serviceCategory.GetAllAsync("/api/v1/category"), "Id", "Name");
-        return View();
+
+            var result = await _serviceProduct.GetByIdAsync("/api/v1/product", id);
+
+            if (result == null)
+                return View("Error");
+
+            return View(result);
+        }
+        catch (Exception)
+        {
+            return View("Error");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        try
+        {
+            var result = await _serviceProduct.GetByIdAsync("/api/v1/product", id);
+
+            if (result == null)
+                return View("Error");
+
+            return View(result);
+        }
+        catch (Exception)
+        {
+            return View("Error");
+        }
+    }
+
+    [HttpPost, ActionName("DeleteProduct")]
+    public async Task<IActionResult> DeleteProductById(int id)
+    {
+        try
+        {
+            var result = await _serviceProduct.DeleteAsync($"/api/v1/product/{id}");
+
+            if (!result)
+                return View("Error");
+
+            return RedirectToAction("Index");
+        }
+        catch (Exception)
+        {
+            return View("Error");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateProduct(ProductViewModel model)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _serviceProduct.PutAsync($"/api/v1/product/{model.Id}", model);
+
+                if (result != null)
+                    return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        catch (Exception)
+        {
+            return View("Error");
+        }
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateProduct(ProductViewModel model)
     {
-        if (ModelState.IsValid)
+        try
         {
-            var result = await _serviceProduct.PostAsync("/api/v1/product", model);
+            if (ModelState.IsValid)
+            {
+                var result = await _serviceProduct.PostAsync("/api/v1/product", model);
 
-            if (result != null)
-            {
-                return RedirectToAction("Index");
+                if (result != null)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.CategoryId = new
+                           SelectList(await _serviceCategory.GetAllAsync("/api/v1/category"), "CategoryId", "Name");
+                }
             }
-            else
-            {
-                ViewBag.CategoryId = new
-                       SelectList(await _serviceCategory.GetAllAsync("/api/v1/category"), "CategoryId", "Name");
-            }
+            return View(model);
         }
-        return View(model);
+        catch (Exception)
+        {
+            return View("Error");
+        }
     }
 }
