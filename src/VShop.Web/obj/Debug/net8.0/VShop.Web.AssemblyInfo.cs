8e4b8f7ed1da96a37426c3b57e579a4e7212cac3
Gabriel Silva <gabriel.silva@JNMOURA.JNMOURA>
2024-04-16 16:43:24 -0300

    Arquivos de debug e build

--- src/VShop.Web/obj/Debug/net8.0/VShop.Web.AssemblyInfo.cs
+++ src/VShop.Web/obj/Debug/net8.0/VShop.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VShop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266d0c2571ed7501d5b818215f63ae7209550c40")]
 [assembly: System.Reflection.AssemblyProductAttribute("VShop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VShop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
